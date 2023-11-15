97d7bb29dca7984a1df9b26eecd869d75a8f2521
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2023-11-14 23:57:49 -0300

    ajuste resposividade footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import './globals.css'
 import Header from '@/app/components/header/Header'
 import Footer from '@/app/components/footer/Footer'
 import Head from '@/app/head'
-import Styles from '@/app/page.module.css'
+
 
 
 export default function RootLayout ( { children } )
@@ -12,7 +12,7 @@ export default function RootLayout ( { children } )
       <Head >
         
       </Head>
-      <body className={Styles.body} > 
+      <body > 
         <Header />
         { children }
         <Footer />
